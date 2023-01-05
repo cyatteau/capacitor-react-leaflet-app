7469a25ff7662f3e9f2576593bc77a35127a1014
cyatteau <112517097+cyatteau@users.noreply.github.com>
2023-01-04 19:16:31 -0500

    Create Simple Leaflet Map with React + Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'capacitor-esri-leaflet-react-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useRef } from "react";
+import { Map as LeafletMap, TileLayer } from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
+  let mapRef = useRef(null);
+
+  useEffect(() => {
+    if (mapRef.current) {
+      let map = new LeafletMap(mapRef.current);
+      map.setView([38.85, -78.2], 8);
+      new TileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+    }
+    return () => {
+      if (mapRef.current) {
+        mapRef.current = null;
+      }
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="map" ref={mapRef}></div>
     </div>
   );
 }
